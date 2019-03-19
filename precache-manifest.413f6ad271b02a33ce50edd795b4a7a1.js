3657517352359e296cf44eab7c1ed1c4e97b423f
Josh Romea <josh.romea@gmail.com>
2019-03-18 23:20:48 -0700

    Updates

---
+++ precache-manifest.413f6ad271b02a33ce50edd795b4a7a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb1c78ba200c73455a2e",
+    "url": "/dbclickygame/static/js/runtime~main.c44bb786.js"
+  },
+  {
+    "revision": "2a0c6d96ac18afc78b8b",
+    "url": "/dbclickygame/static/js/main.364d659b.chunk.js"
+  },
+  {
+    "revision": "62507dd0e61ef6a60d15",
+    "url": "/dbclickygame/static/js/2.c4d713e9.chunk.js"
+  },
+  {
+    "revision": "2a0c6d96ac18afc78b8b",
+    "url": "/dbclickygame/static/css/main.fb681f74.chunk.css"
+  },
+  {
+    "revision": "aa3a1513d049c5937ab267bc44f88840",
+    "url": "/dbclickygame/index.html"
+  }
+];
